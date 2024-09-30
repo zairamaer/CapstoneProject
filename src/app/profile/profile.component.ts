0df78aed428f868e6df340674896e74d9021b612
Zaira Mae Reyes <zairamaer@gmail.com>
2024-09-30 12:55:35 +0800

    add static data for user

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -5,16 +5,14 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss'],
 })
-export class ProfileComponent  implements OnInit {
+export class ProfileComponent implements OnInit {
+  user = {
+    name: 'John Doe',         // Static name
+    email: 'johndoe@example.com', // Static email
+    phone: '123-456-7890'     // Static phone number
+  };
 
   constructor() { }
 
-  user = {
-    name: '',
-    email: '',
-    phone: ''
-  };
-
   ngOnInit() {}
-
 }
